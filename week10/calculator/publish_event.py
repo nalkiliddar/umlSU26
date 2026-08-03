bd04396e00d7579af72b34640c50adb37a85ed3e
nalkiliddar <Noor_AlKiliddar@student.uml.edu>
2026-08-02 20:04:36 -0400

    Update publish_event.py

--- week10/calculator/publish_event.py
+++ week10/calculator/publish_event.py
@@ -6,7 +6,7 @@ kafka-python image, after the image has been built and pushed.
 TODO (Exercise 10, Part 2): the consumer needs to know WHICH version to test and
 promote. Add the build's version to the event below. The pipeline builds
 calculator:<build number> and provides that number as the VERSION env var.
-OK
+"OK"
 """
 import json
 import os
