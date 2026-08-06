0ccb56c643262129f4789387fd643b7fb14d0d0a
nalkiliddar <Noor_AlKiliddar@student.uml.edu>
2026-08-05 20:02:34 -0400

    Update CalculatorTest.java

--- week10/calculator/src/main/test/java/com/leszko/calculator/CalculatorTest.java
+++ week10/calculator/src/main/test/java/com/leszko/calculator/CalculatorTest.java
@@ -3,9 +3,9 @@ package com.leszko.calculator;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals; 
 
-/** Test for Calculator logic */
+
 @SpringBootTest
 public class CalculatorTest {
 
@@ -13,20 +13,20 @@ public class CalculatorTest {
 	private Calculator calculator;
 
 	@Test
-	public void testSumPositiveNumbers() {
-		assertThat(calculator.sum(2, 3)).isEqualTo(5);
+	public void SumPositiveNumbers() {
+		Assert.assertEquals(5, calculator.sum(2, 3));
 	}
 
 	@Test
-	public void testSumZero() {
-		assertThat(calculator.sum(0, 0)).isEqualTo(0);
-		assertThat(calculator.sum(5, 0)).isEqualTo(5); 
+	public void SumZero() {
+		Assert.assertEquals(0, calculator.sum(0, 0));
+		Assert.assertEquals(5, calculator.sum(5, 0)); 
 		
 	}
 
 	@Test
 	public void testSumNegativeNumbers() {
-		assertThat(calculator.sum(-2, -3)).isEqualTo(-5);
-		assertThat(calculator.sum(2, -3)).isEqualTo(-1);
+		Assert.assertEquals(-5, calculator.sum(-2, -3));
+		Assert.assertEquals(-1, calculator.sum(2, -3));
 	}
 }
